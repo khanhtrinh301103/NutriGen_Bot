8f8a7abc7f9062e08c09c6ab6db3851e34313b8e
khanhtrinh301103 <s3927152@rmit.edu.vn>
2025-04-30 00:49:25 +0700

    update set up to deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,10 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -10,6 +14,14 @@ const nextConfig = {
     };
     return config;
   },
+  // Chỉ áp dụng khi deploy lên Netlify
+  ...(process.env.NETLIFY === 'true' && {
+    staticPageGenerationTimeout: 120,
+    experimental: {
+      // Tắt một số chức năng có thể gây lỗi
+      esmExternals: 'loose',
+    }
+  })
 };
 
 export default nextConfig;
